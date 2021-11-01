3428f0af43a60647ba09f051e7f761e6ac9557b2
jotas <jotasenator@gmail.com>
2021-10-31 21:31:05 -0400

    Add provider configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
