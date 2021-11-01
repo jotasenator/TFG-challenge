62916fc51150995236bf87ea16285ce420b9d677
jotas <jotasenator@gmail.com>
2021-10-31 21:26:26 -0400

    Add redux element for state sharing

---
+++ src/redux/reducers/axiosDataReducer.js
@@ -0,0 +1,49 @@
+import { types } from "../types/types";
+
+const initialState = {
+    data: null,
+    selected: null,
+    deleted: '',
+    created: null,
+    modified: null,
+    error: ''
+}
+
+export const axiosDataReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.get:
+            return {
+                ...state,
+                data: action.data
+            }
+        case types.selected:
+            return {
+                ...state,
+                selected: action.selectedItem
+            }
+        case types.delete:
+            return {
+                ...state,
+                data: state.data.filter(item => item.id !== action.deletedItem.id),
+                deleted: action.deletedItem
+            }
+        case types.created:
+            return {
+                ...state,
+                created: action.createdItem
+            }
+        case types.modified:
+            return {
+                ...state,
+                modified: action.modifiedItem
+            }
+
+        case types.error:
+            return {
+                ...state,
+                error: action.msg
+            }
+        default:
+            return state;
+    }
+}
---
+++ src/redux/types/types.js
@@ -0,0 +1,9 @@
+export const types = {
+    get: 'GET-DATA',
+    error: 'ERROR',
+    selected: 'SELECTED-DATA',
+    delete: 'DELETED-DATA',
+    created: 'CREATED-DATA',
+    modify: 'MODIFIED-DATA',
+
+}
